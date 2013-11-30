ff5e8cc5192a224b409d6b1eee68a1d4e95b43b3
vitaliy <vitaliy.yxz@gmail.com>
2013-12-01 01:20:35 +0200

    - update template and style

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,28 @@
 (function(global,undefined){
 
-    jQuery(document).on('click','a[href*="#"]', function(e) {
-        location.hash = this.href.split("#")[1];
+//    jQuery(document).on('click','a[href*="#"]', function(e) {
+//        location.hash = this.href.split("#")[1];
+//        return false;
+//    });
+    jQuery( "body>[data-role='panel']" ).panel();
+
+
+
+    jQuery( ".main_menu-link" ).on( "click", function() {
+        jQuery( "#main_menu" ).panel( "open" );
         return false;
     });
 
+    jQuery( function() {
+        $(".hide").removeClass("hide");
+        $( "#cooment_popup" ).enhanceWithin().popup({
+            align: "0.5,0.5"
+        });
+    });
+
     setTimeout(function(){
-        location.hash = "#main_page";
-    },100);
+        location.hash = "#auth_page";
+    },0);
 
     global.app = angular.module('myApp',[]);
 })(window);
