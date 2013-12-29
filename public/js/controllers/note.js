8fd46fced344fd201383573b12111978364a5caa
vitaliy <vitaliy.yxz@gmail.com>
2013-12-30 00:04:22 +0200

    - add code comments

--- public/js/controllers/note.js
+++ public/js/controllers/note.js
@@ -7,6 +7,9 @@ function NoteCtrl($scope, connect, navigation, $rootScope, $sce, timeconverter) 
         isLoadData = false;
     $scope.select_note = {};
 
+    /**
+     * before enter to page trigger
+     */
     navigation.beforePageChange("note_page",function(){
         if(!isLoadData){
             connect.getNote(function(err,data){
@@ -17,24 +20,30 @@ function NoteCtrl($scope, connect, navigation, $rootScope, $sce, timeconverter) 
                 isLoadData = true;
                 setBlock();
                 $scope.nodes = data;
-                console.log("note_page data update",data,$rootScope.select_date);
-                alert(JSON.stringify(data));
-                alert($rootScope.select_date);
+                //console.log("note_page data update",data,$rootScope.select_date);
                 selectTimestamp($rootScope.select_date);
                 $scope.$apply();
             });
         }
     });
 
+    /**
+     * trigger for synchronize event
+     */
     $rootScope.$on('sync', function(){
         isLoadData = false;
     });
 
+
     $rootScope.$watch('select_date',selectTimestamp);
 
+    /**
+     * handler on select timestamp
+     * @param newDate
+     */
     function selectTimestamp(newDate){
         if($scope.nodes){
-            var newTimestamp = timeconverter.convertDayToTimestamp(newDate);
+            var newTimestamp = timeconverter.convertDayToTimestamp(newDate) - 12 * 60 * 60;
             if(!$scope.nodes[newTimestamp]){
                 $scope.nodes[newTimestamp] = {
                     date: $rootScope.select_date,
@@ -47,11 +56,17 @@ function NoteCtrl($scope, connect, navigation, $rootScope, $sce, timeconverter) 
         }
     }
 
+    /**
+     * stop listen change note for small time (need for update data)
+     */
     function setBlock(){
         block = true;
         setTimeout(function(){ block = false; },200);
     }
 
+    /**
+     * listen change note for mark  note as changed (and need save to server)
+     */
     $scope.$watch('select_note.note',function(){
         if(!block){
             $scope.select_note.$$_status = "updated";
